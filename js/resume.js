c17534daa5b92b2bb57ee450b2ae11adaad00703
LazezBZH <lazez.bzh@caramail.fr>
2023-10-30 00:21:31 +0100

    corr fo cv and resp

--- js/resume.js
+++ js/resume.js
@@ -2,7 +2,6 @@ const cvEmbed = document.querySelector(".cvEmbed ");
 const cvBtn = document.querySelector(".cvBtn");
 const closeEmbed = document.querySelector(".closeEmbed");
 
-cvBtn.addEventListener("click", showCV);
 closeEmbed.addEventListener("click", hideCV);
 
 function showCV() {
@@ -12,3 +11,20 @@ function showCV() {
 function hideCV() {
   cvEmbed.style.display = "none";
 }
+function isMobileDevice() {
+  if (
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/Android/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+  ) {
+    cvBtn.style.display = "none";
+  } else {
+    cvBtn.style.display = "block";
+    cvBtn.addEventListener("click", showCV);
+  }
+}
+isMobileDevice();
